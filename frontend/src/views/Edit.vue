cbc5bff9e04446d44d927771d2530ef6d449a3a1
edemkaaa <edemasanov2004@gmail.comgit add .git commit -m Create>
2023-01-12 19:23:52 +0300

    Конец проекта

---
+++ frontend/src/views/Edit.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <EditProduct />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EditProduct from "@/components/EditProduct.vue";
+export default {
+    name: "Edit",
+    components: {
+        EditProduct,
+    },
+};
+</script>
---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ProductList />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductList from "@/components/ProductList.vue";
+export default {
+    name: "Index",
+    components: {
+        ProductList,
+    },
+};
+</script>
